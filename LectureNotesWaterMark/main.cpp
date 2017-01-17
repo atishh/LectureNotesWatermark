ab8c6619d32f9f1560bc9b1f54889e27735ba772
atish <7705506+atishh@users.noreply.github.com>
2017-01-18 00:42:52 +0530

    Porting to linux

--- LectureNotesWaterMark/main.cpp
+++ LectureNotesWaterMark/main.cpp
@@ -4,9 +4,16 @@
 #include<opencv2/highgui/highgui.hpp>
 #include<opencv2/imgproc/imgproc.hpp>
 
-#include <fstream>
+#include<fstream>
 #include<iostream>
+#ifdef __linux__ 
+    //linux code goes here
+#elif _WIN32
+    // windows code goes here
 #include<conio.h>           // it may be necessary to change or remove this line if not using Windows
+#else
+#endif
+
 #include "Magick++.h"
 
 /// Global Variables
